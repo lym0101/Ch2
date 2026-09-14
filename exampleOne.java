f8a2c63318d359adb15a143c4e3db04ce16882b2
Eric Li <65319100+lym0101@users.noreply.github.com>
2026-09-14 14:57:16 -0700

    Add files via upload

---
+++ exampleOne.java
@@ -0,0 +1,7 @@
+public class exampleOne {
+	public static void main (String[] args) {
+	String name = "Eric";
+	int age = 17;
+	System.out.print("My name is "+ name + " and I am " + age + ".");
+	}
+}
---
+++ exampleTwo.java
@@ -0,0 +1,7 @@
+public class exampleTwo {
+	public static void main (String[] args) {
+	System.out.println(7/2); // --> 3
+	System.out.println(7.0/2); // --> 3.5
+	System.out.println(7%2); //--> 1
+	}
+}
